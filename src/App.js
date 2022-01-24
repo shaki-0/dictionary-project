97725443db7a7e90112c1e9abbe731a8bda7d846
shaki-0 <shakiobadina28@gmail.com>
2022-01-24 16:47:04 +0000

    Created react app and deleted unnecessary files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1> Enter a text </h1>
+    </div>
+  );
+}
+
+
