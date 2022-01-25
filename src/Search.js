417d5fb41d2eed2696bf0c111e01ec4aa54423d0
shaki-0 <shakiobadina28@gmail.com>
2022-01-25 15:44:59 +0000

    Axios, api intergration and search engine
    1. Installed and imported axios for my project.
    2. I added the API url to my project and created a function for the API call.
    3.I interpolated the state I created into the apiURL so that when a user types in a word the definition will be returned.
    4.Also created a search engine and added two event listeners.

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Search() {
-const [word,setWord] = useState ("");
-
-    function result(event) {
-        event.preventDefault();
-        alert (`Searching for ${word}`); 
-    }
-
-    function handleWordChange (event) {
-        setWord(event.target.value);
-    }
+  const [word, setWord] = useState("");
 
 
-  return (
+  function getResponse (response) {
+    console.log(response.data[0]);
+  }
+
+  function result(event) {
+    event.preventDefault();
+
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(getResponse);
+  }
+
+  function handleWordChange(event) {
+    setWord(event.target.value);
+  }
+
+ return (
     <div className="Search">
       <form onSubmit={result}>
-          <input type="search" placeholder="Enter a word" onChange={handleWordChange} />
+        <input
+          type="search"
+          placeholder="Enter a word"
+          onChange={handleWordChange}
+        />
       </form>
     </div>
   );
