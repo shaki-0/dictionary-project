2c127cc72232ae00aab8ef5cbdbc68e9727b2e38
shaki-0 <shakiobadina28@gmail.com>
2022-01-31 15:24:31 +0000

    Added synonyms to my project
    1. I created a new component called synonyms.
    2. The API does not show the synonyms for all definitions. This is why in the Synonyms component  I used conditional rendering.
    3. I also used a loop for the synonyms so that I did not have to repeat the code.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
-
-export default function Meaning  (props) {
-    return (
-        <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function (definition, index)
-        {
-        return (<div key={index}>
-                <p> 
-                    {definition.definition}
-                    <br />
-                    <em> {definition.example} </em>
-                </p>
-            </div>
-             );
-            })}
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <strong> Example: </strong>
+              <em> {definition.example} </em>
+              <Synonyms synonyms = {definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
     </div>
-    );
+  );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Meaning from "./Meaning"; 
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2> {props.results.word} </h2>
-        <p>Definition </p>
+        <br />
+        <strong>Definition</strong>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
