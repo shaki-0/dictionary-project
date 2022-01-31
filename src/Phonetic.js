1c54bc2cfa36659ee402dccd526c1bdf00761cf3
shaki-0 <shakiobadina28@gmail.com>
2022-01-31 17:41:19 +0000

    Added phonetics and audio to my project
    1. In the phonetics component I wanted the user to click on the button and the phonetic of the word would play. To do this I used a hook for the audio. In the parenthesis I added the phonetic property and targeted the object key at the end which is called audio. The object key comes from the API.
    2. I used the event listener onClick and created a function for it.
    3. I installed Font Awesome packages so that I could use the volume up icon.
    4. I imported Font Awesome and wrapped it around a button element.
    5. I added the phonetic text from the API and wrapped it around a paragraph element.

---
+++ src/Phonetic.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faVolumeUp} from "@fortawesome/free-solid-svg-icons";
+
+export default function Phonetic(props) {
+  const audioElement = new Audio(props.phonetic.audio);
+
+  function playSound(event) {
+    audioElement.play();
+  }
+
+  return (
+    <div className="Phonetic">
+      <button onClick={playSound}>
+         <FontAwesomeIcon icon={faVolumeUp} />
+      </button>
+    <br />
+   <p> {props.phonetic.text} </p>
+</div>
+  );
+}
