aed2d032e06eb8cd586a4cd56ed5fa1860d286c7
shaki-0 <shakiobadina28@gmail.com>
2022-02-02 18:57:21 +0000

    Added https to custom font link because it did not deploy on Netlify

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import Results from "./Results";
-import "./Search.css"
+import "./Search.css";
 import axios from "axios";
 
 export default function Search() {
   const [word, setWord] = useState("");
-  const [definitions,setDefinitions] = useState (null);
+  const [definitions, setDefinitions] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
-
-  function getResponse (response) {
+  function getResponse(response) {
     setDefinitions(response.data[0]);
   }
 
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
+    search();
+  }
 
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(getResponse);
   }
@@ -24,19 +26,29 @@ export default function Search() {
     setWord(event.target.value);
   }
 
- return (
-   <div className="Search">
-     <div className="secondSection">
-       <form onSubmit={search}>
-         <input
-           className="typeWord"
-           type="search"
-           placeholder="Type a word"
-           onChange={handleWordChange}
-         />
-       </form>
-     </div>
-     <Results results={definitions} />
-   </div>
- );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Search">
+        <div className="secondSection">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="typeWord"
+              type="search"
+              placeholder="Type a word"
+              onChange={handleWordChange}
+            />
+          </form>
+        </div>
+        <Results results={definitions} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
